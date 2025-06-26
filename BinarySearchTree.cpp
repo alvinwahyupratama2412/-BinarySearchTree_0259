4c6e5646689c76ec212539081c3e92993bb92385
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-06-26 10:56:39 +0700

    membuat fungsi inorder (left-ROOT-right)

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -80,6 +80,16 @@ class BinaryTree{
                 currentNode = currentNode->rightchild;
         }
     }
+ void inorder(Node *ptr){
+        if (isEmpty()){
+            cout << "Tree is empty." << endl;
+            return;
+        }
+        if (ptr == nullptr)
+            return;
 
-   
+            inorder(ptr->leftchild);
+            cout << ptr->info << " ";
+            inorder(ptr->rightchild);
+    }
 };
