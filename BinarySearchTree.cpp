fdce1cf62401d02caf7344153e91863c6317ad03
alvinwahyupratama2412 <alvinwahyupratama22@gmail.com>
2025-06-26 10:38:50 +0700

    jika parent NULL maka menandai newNode sebagai ROOT

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -42,6 +42,15 @@ class BinaryTree{
         Node *currentNode = nullptr;
         search (x, parent, currentNode);
 
+        //Step 5 : if parent is NULL(tree is empty)
+        if (parent == nullptr) {
+            //5a : Mark the new node as ROOT
+            ROOT = newNode;
+
+            //5b : Exit
+            return; 
+        }
+    
        
     }
 
